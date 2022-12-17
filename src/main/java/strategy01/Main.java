3e6d1c1ef9af1a1fcd37ddfb36e1fe1691498484
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2022-12-17 17:07:44 -0300

    Improve interface.

--- src/main/java/strategy01/Main.java
+++ src/main/java/strategy01/Main.java
@@ -1,12 +1,19 @@
 package strategy01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Integer numKilometers = Integer.parseInt(args[0]);
-		TypeFreight typeFreight = TypeFreight.valueOf(args[1]);
-		Freight freight = typeFreight.instance();
-		System.out.println(freight.calculate(numKilometers));
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("How many quilometers? ");
+			Integer numKilometers = scanner.nextInt();
+			System.out.println("Type of freight? ");
+			TypeFreight typeFreight = TypeFreight.valueOf(scanner.next());
+			
+			Freight freight = typeFreight.instance();
+			System.out.println(freight.calculate(numKilometers));
+		}
 	}
 
 }
